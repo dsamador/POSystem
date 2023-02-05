3ac9801aa9574963f03f964dafe71c401e0f7bf7
David Amador <54488581+dsamador@users.noreply.github.com>
2023-02-04 21:54:10 -0500

    Messages

---
+++ POS.Utilities/Static/ReplyMessage.cs
@@ -0,0 +1,16 @@
+﻿namespace POS.Utilities.Static
+{
+    public class ReplyMessage
+    {
+        public const string MESSAGE_QUERY = "Consulta exitosa";
+        public const string MESSAGE_QUERY_EMPTY = "No se encotraron registros";
+        public const string MESSAGE_SAVE = "Se registro correctamente";
+        public const string MESSAGE_UPDATE = "Se actualizo correctamente";
+        public const string MESSAGE_DELETE = "Se elimino correctamente";
+        public const string MESSAGE_EXISTS = "El registro ya existe";
+        public const string MESSAGE_ACTIVATE = "El registro ha sido activado";
+        public const string MESSAGE_TOKEN = "Token generado correctamente";
+        public const string MESSAGE_VALIDATE = "Hay errores de validacion";
+        public const string MESSAGE_FAILED = "Operacion fallida";
+    }
+}
